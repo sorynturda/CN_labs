adee552c3a8d308655e3346e3c9a0bacad0d679e
Sorin Turda <s.turda12@gmail.com>
2024-03-15 13:21:55 +0200

    hermite si lagrange interpolare

---
+++ 02/INTERPOLARE-LAGRANGE/Interval_interpolare.m
@@ -0,0 +1,8 @@
+function [a,b]=Interval_interpolare(tipex)
+%functie care "returneaza" intervalul de interpolare
+switch tipex
+    case 'ex1'
+        a=-1;b=1;
+    case 'ex2'
+        a=-5;b=5;
+end
---
+++ 02/INTERPOLARE-LAGRANGE/fL.m
@@ -0,0 +1,8 @@
+function y=fL(x,tipex)
+%functie care "returneaza" valorile functiei in punctul x (teoretic functia nu se cunoaste, doar anumite valori-se foloseste pentru verificari)
+switch tipex
+    case 'ex1'
+       y=sin(2*pi*x);
+    case 'ex2'
+        y=1./(1+x.^2); 
+ end
